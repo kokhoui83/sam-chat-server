512f5256222c21a2b45e33b1434b1130831af010
brian lim <kokhoui83@gmail.com>
2020-10-23 22:32:25 +0800

    add postChat function

--- chat/app.py
+++ chat/app.py
@@ -2,12 +2,20 @@ import json
 
 from database import retrieveChat
 
-def getChats(event, context):
+def getChat(event, context):
     chats = retrieveChat()
     return {
         "statusCode": 200,
         "body": json.dumps({
             "status": "ok",
             "chats": chats
+        })
+    }
+
+def postChat(event, context):
+    return {
+        "statusCode": 201,
+        "body": json.dumps({
+            "status": "ok"
         })
     }
